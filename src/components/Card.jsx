fa92bd98672308d7c39656231f8953cba7723db6
Spike Vinz Cruz <spikevinz@gmail.com>
2021-09-16 08:36:24 +0800

    add hover animation

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,8 @@ import React from "react";
 export default function Card(props) {
   return (
     <div
-      className=" flex items-center justify-center bg-blue-300"
+      className=" flex items-center justify-center bg-blue-300
+       cursor-pointer hover:shadow-2xl transition-transform transform hover:scale-110 "
       onClick={() => props.handleClick(props.id)}
     >
       <div className="">
