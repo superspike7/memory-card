61d6b6f33208c93a15f15fefa49256cde818798d
Spike Vinz Cruz <spikevinz@gmail.com>
2021-09-16 08:15:27 +0800

    add card image

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,21 @@ import Nav from "./components/Nav";
 import Card from "./components/Card";
 
 const cardsJson = [
-  { id: 1, img: "1" },
-  { id: 2, img: "2" },
-  { id: 3, img: "3" },
-  { id: 4, img: "4" },
-  { id: 5, img: "5" },
-  { id: 6, img: "6" },
-  { id: 7, img: "7" },
-  { id: 8, img: "8" },
-  { id: 9, img: "9" },
-  { id: 10, img: "10" },
+  { id: 1, img: "https://comicvine.gamespot.com/a/uploads/square_small/11117/111178336/7080194-7382724028-unkno.png" },
+  { id: 2, img: "https://comicvine.gamespot.com/a/uploads/square_small/11117/111178336/7080203-3155899631-unkno.png" },
+  { id: 3, img: "https://comicvine.gamespot.com/a/uploads/square_small/11117/111178336/7080201-7164581366-unkno.png" },
+  { id: 4, img: "https://comicvine.gamespot.com/a/uploads/square_small/4/47703/2434111-473592_sanji_time_skip_1.png" },
+  { id: 5, img: "https://comicvine.gamespot.com/a/uploads/square_small/4/47703/2434105-render_nicorobin.png" },
+  { id: 6, img: "https://comicvine.gamespot.com/a/uploads/square_small/4/47703/2434113-chopper_time_skip_2.png" },
+  { id: 7, img: "https://comicvine.gamespot.com/a/uploads/square_small/4/47703/2434115-473629_untitled_2.png" },
+  { id: 8, img: "https://comicvine.gamespot.com/a/uploads/square_small/4/47703/2434117-473497_brook_timeskip1.png" },
+  { id: 9, img: "https://comicvine.gamespot.com/a/uploads/square_small/4/47703/2841730-law.jpg" },
+  { id: 10, img: "https://comicvine.gamespot.com/a/uploads/square_small/11117/111178336/7078671-9363995852-39fc3.jpg" },
+  { id: 11, img: "https://comicvine.gamespot.com/a/uploads/square_small/4/47703/3056239-7978640494-Jinbe.png" },
+  { id: 12, img: "https://comicvine.gamespot.com/a/uploads/square_small/4/46706/1152176-current_blackbeard.png" },
+  { id: 13, img: "https://comicvine.gamespot.com/a/uploads/square_small/11117/111178336/7080183-4615055614-unkno.png" },
+  { id: 14, img: "https://comicvine.gamespot.com/a/uploads/square_small/5/57216/1090020-61600.jpg" },
+  { id: 15, img: "https://comicvine.gamespot.com/a/uploads/square_small/11117/111178336/7080202-1635462455-unkno.png" },
 ];
 
 function App() {
@@ -23,9 +28,10 @@ function App() {
 
   useEffect(() => {
     if (hasDuplicateVal(clicked)) {
-      if (!alert("You Lose!")) {
+      if (!alert("You Lose! You clicked the the image twice!")) {
         setCurrentScore(-1);
         setClicked([]);
+        setCards(shuffle(cards))
       }
     } else if (!hasDuplicateVal(clicked)) {
       setCurrentScore(currentScore + 1);
@@ -37,7 +43,7 @@ function App() {
       setHighScore(currentScore);
     }
 
-    if(currentScore === 10){
+    if(currentScore === cards.length){
       alert("Congrats you win!");
     }
   }, [currentScore]);
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,10 +3,12 @@ import React from "react";
 export default function Card(props) {
   return (
     <div
-      className="h-32 flex items-center justify-center bg-blue-300"
+      className=" flex items-center justify-center bg-blue-300"
       onClick={() => props.handleClick(props.id)}
     >
-      <div className="text-xl">{props.img}</div>
+      <div className="">
+        <img src={props.img} alt="" className="object-fit" />
+      </div>
     </div>
   );
 }
